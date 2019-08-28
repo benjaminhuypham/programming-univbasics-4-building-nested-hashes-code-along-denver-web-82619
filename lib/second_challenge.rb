3d18e1af2d9bed5514763e0fc54cdbb1325bb67a
Huy Pham <benjaminhuypham@gmail.com>
2019-08-28 07:59:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,6 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      
       }, 
     :capulet => {
       #your key/value pairs here!
